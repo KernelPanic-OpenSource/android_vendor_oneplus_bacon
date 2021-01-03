9823bb52e2dc7925f8ec605f9e12fe25606692a7
dopaemon <polarisdp@gmail.com>
2021-01-03 15:57:01 +0700

    halium: Remove Java (QcRilMsgTunnel)
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- Android.mk
+++ Android.mk
@@ -151,17 +151,5 @@ LOCAL_MODULE_SUFFIX := .so
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := qcrilmsgtunnel
-LOCAL_MODULE_OWNER := oneplus
-LOCAL_SRC_FILES := proprietary/priv-app/qcrilmsgtunnel/qcrilmsgtunnel.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_PRIVILEGED_MODULE := true
-include $(BUILD_PREBUILT)
-
 endif
 endif
--- bacon-vendor.mk
+++ bacon-vendor.mk
@@ -205,6 +205,5 @@ PRODUCT_PACKAGES += \
     libqmi \
     libqmi_client_qmux \
     libqmiservices \
-    libtime_genoff \
-    qcrilmsgtunnel
+    libtime_genoff
 endif
